9804559d6d3405b658613d598a9b5503d5532e69
barbara-musial <basia.musial22@op.pl>
2021-09-26 14:38:54 +0200

    Add clear function

--- main.py
+++ main.py
@@ -1,40 +1,59 @@
 import tkinter as tk
 
+
+def click(number):
+    e.insert(0, number)
+
+
+def clear():
+    e.delete(0, 'end')
+
+
 win = tk.Tk()
 win.title('Calculator')
 win.geometry('250x350')
 
-label = tk.Label(win)
-label.grid(column=0, columnspan=4, row=0, sticky='NSEW')
+e = tk.Entry(win, text='')
+e.grid(column=0, columnspan=4, row=0, sticky='NSEW')
 
-b1 = tk.Button(win, text='1')
+b1 = tk.Button(win, text='1',
+               command=lambda: click(1))
 b1.grid(column=0, row=2, sticky='NSEW')
 
-b2 = tk.Button(win, text='2')
+b2 = tk.Button(win, text='2',
+               command=lambda: click(2))
 b2.grid(column=1, row=2, sticky='NSEW')
 
-b3 = tk.Button(win, text='3')
+b3 = tk.Button(win, text='3',
+               command=lambda: click(3))
 b3.grid(column=2, row=2, sticky='NSEW')
 
-b4 = tk.Button(win, text='4')
+b4 = tk.Button(win, text='4',
+               command=lambda: click(4))
 b4.grid(column=0, row=3, sticky='NSEW')
 
-b5 = tk.Button(win, text='5')
+b5 = tk.Button(win, text='5',
+               command=lambda: click(5))
 b5.grid(column=1, row=3, sticky='NSEW')
 
-b6 = tk.Button(win, text='6')
+b6 = tk.Button(win, text='6',
+               command=lambda: click(6))
 b6.grid(column=2, row=3, sticky='NSEW')
 
-b7 = tk.Button(win, text='7')
+b7 = tk.Button(win, text='7',
+               command=lambda: click(7))
 b7.grid(column=0, row=4, sticky='NSEW')
 
-b8 = tk.Button(win, text='8')
+b8 = tk.Button(win, text='8',
+               command=lambda: click(8))
 b8.grid(column=1, row=4, sticky='NSEW')
 
-b9 = tk.Button(win, text='9')
+b9 = tk.Button(win, text='9',
+               command=lambda: click(9))
 b9.grid(column=2, row=4, sticky='NSEW')
 
-b0 = tk.Button(win, text='0')
+b0 = tk.Button(win, text='0',
+               command=lambda: click(0))
 b0.grid(column=0, columnspan=3, row=5, sticky='NSEW')
 
 plus = tk.Button(win, text='+')
@@ -52,7 +71,7 @@ divide.grid(column=1, row=1, sticky='NSEW')
 equal = tk.Button(win, text='=')
 equal.grid(column=3, row=4, rowspan=2, sticky='NSEW')
 
-c = tk.Button(win, text='C')
+c = tk.Button(win, text='C', command=clear)
 c.grid(column=0, row=1, sticky='NSEW')
 
 win.grid_columnconfigure(0, weight=1)
