da29540f3b0ffd88bdf78889746c04a7ddcf5a82
barbara-musial <basia.musial22@op.pl>
2021-09-29 12:35:17 +0200

    Add label

--- main.py
+++ main.py
@@ -59,82 +59,86 @@ win = tk.Tk()
 win.title('Calculator')
 win.geometry('250x350')
 
+l2 = tk.Label(win, text='dtjyg', anchor='ne', bg='#e6f7ff', font=('Helvatical bold', 10))
+l2.grid(column=0, columnspan=4, row=0, sticky='NSEW')
+
 e = tk.Entry(win, text='', justify='right', bg='#e6f7ff', font=('Helvatical bold', 20))
-e.grid(column=0, columnspan=4, row=0, sticky='NSEW')
+e.grid(column=0, columnspan=4, row=1, sticky='NSEW')
 
 b1 = tk.Button(win, text='1', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(1))
-b1.grid(column=0, row=2, sticky='NSEW')
+b1.grid(column=0, row=3, sticky='NSEW')
 
 b2 = tk.Button(win, text='2', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(2))
-b2.grid(column=1, row=2, sticky='NSEW')
+b2.grid(column=1, row=3, sticky='NSEW')
 
 b3 = tk.Button(win, text='3', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(3))
-b3.grid(column=2, row=2, sticky='NSEW')
+b3.grid(column=2, row=3, sticky='NSEW')
 
 b4 = tk.Button(win, text='4', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(4))
-b4.grid(column=0, row=3, sticky='NSEW')
+b4.grid(column=0, row=4, sticky='NSEW')
 
 b5 = tk.Button(win, text='5', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(5))
-b5.grid(column=1, row=3, sticky='NSEW')
+b5.grid(column=1, row=4, sticky='NSEW')
 
 b6 = tk.Button(win, text='6', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(6))
-b6.grid(column=2, row=3, sticky='NSEW')
+b6.grid(column=2, row=4, sticky='NSEW')
 
 b7 = tk.Button(win, text='7', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(7))
-b7.grid(column=0, row=4, sticky='NSEW')
+b7.grid(column=0, row=5, sticky='NSEW')
 
 b8 = tk.Button(win, text='8', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(8))
-b8.grid(column=1, row=4, sticky='NSEW')
+b8.grid(column=1, row=5, sticky='NSEW')
 
 b9 = tk.Button(win, text='9', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(9))
-b9.grid(column=2, row=4, sticky='NSEW')
+b9.grid(column=2, row=5, sticky='NSEW')
 
 b0 = tk.Button(win, text='0', bg='#b3e6ff', activeforeground='#cccccc',
                command=lambda: click(0))
-b0.grid(column=0, columnspan=3, row=5, sticky='NSEW')
+b0.grid(column=0, columnspan=3, row=6, sticky='NSEW')
 
 plus = tk.Button(win, text='+', bg='#66ccff', activeforeground='#cccccc', bd=4,
                  command=add)
-plus.grid(column=3, row=2, rowspan=2, sticky='NSEW')
+plus.grid(column=3, row=3, rowspan=2, sticky='NSEW')
 
 minus = tk.Button(win, text='-', bg='#66ccff', activeforeground='#cccccc', bd=4,
                   command=sub)
-minus.grid(column=3, row=1, sticky='NSEW')
+minus.grid(column=3, row=2, sticky='NSEW')
 
 mult = tk.Button(win, text='×', bg='#66ccff', activeforeground='#cccccc', bd=4,
                  command=multiply)
-mult.grid(column=2, row=1, sticky='NSEW')
+mult.grid(column=2, row=2, sticky='NSEW')
 
 div = tk.Button(win, text='÷', bg='#66ccff', activeforeground='#cccccc', bd=4,
                 command=divide)
-div.grid(column=1, row=1, sticky='NSEW')
+div.grid(column=1, row=2, sticky='NSEW')
 
 equal = tk.Button(win, text='=', bg='#66ccff', activeforeground='#cccccc', bd=4,
                   command=result)
-equal.grid(column=3, row=4, rowspan=2, sticky='NSEW')
+equal.grid(column=3, row=5, rowspan=2, sticky='NSEW')
 
 c = tk.Button(win, text='C', bg='#66ccff', activeforeground='#cccccc', bd=4,
               command=clear)
-c.grid(column=0, row=1, sticky='NSEW')
+c.grid(column=0, row=2, sticky='NSEW')
 
 win.grid_columnconfigure(0, weight=1)
 win.grid_columnconfigure(1, weight=1)
 win.grid_columnconfigure(2, weight=1)
 win.grid_columnconfigure(3, weight=1)
-win.grid_rowconfigure(0, weight=2)
+win.grid_rowconfigure(0, weight=0)
 win.grid_rowconfigure(1, weight=1)
 win.grid_rowconfigure(2, weight=1)
 win.grid_rowconfigure(3, weight=1)
 win.grid_rowconfigure(4, weight=1)
 win.grid_rowconfigure(5, weight=1)
+win.grid_rowconfigure(6, weight=1)
 
 tk.mainloop()
